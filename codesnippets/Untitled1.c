31544b0497bb6220c836c82f78cd8783b4348364
bhuvighosh3 <108955148+bhuvighosh3@users.noreply.github.com>
2022-07-23 09:21:40 +0530

    Update Untitled1.c

--- codesnippets/Untitled1.c
+++ codesnippets/Untitled1.c
@@ -33,9 +33,10 @@ int main()
 {
     int i=0,n,m,j,k;
     struct userinformation s[100];
+    struct excercise e[100];
     int number;
     float h,w,BMR,a,t,calburnt,sum=0,bm;
-    char g,e;
+    char g;
     printf("Enter height in cm:");
     scanf("%f",&h);
     printf("Enter weight in kg:");
@@ -47,57 +48,16 @@ int main()
     scanf("%c",&g);
     BMR=bmr(h,w,a,g,bm);
     printf("Your Basal Metabolic Rate(BMR) is %f",BMR);
-
-     for(i=0;i<10;i++)
-    {
-
-         if(i==0)
-         {
-            e[i].m=0;
-            e[i].exercise=="Swimming";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-
-         else if(i==1)
-         {
-            e[i].m=1;
-            e[i].exercise=="Cycling";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-         else if(i==2)
-         {
-            e[i].m=2;
-            e[i].exercise=="Walking";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-         else if(i==3)
-         {
-            e[i].m=3;
-            e[i].exercise=="Weight Training";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-          else if(i==4)
-         {
-            e[i].m=4;
-            e[i].exercise=="Running";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-          else if(i==5)
-         {
-            e[i].m=5;
-            e[i].exercise=="Jogging";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-          else if(i==6)
-         {
-            e[i].m=6;
-            e[i].exercise=="Aerobics";
-            printf("%d%s",e[i].m,e[i].exercise);
-         }
-
-
-
-    }
+    e[0]={1,"Swimming"};
+    e[1]={2,"Cycling"};
+    e[2]={3,"Running"};
+    e[4]={4,"Weight Training"};
+    e[5]={5,"Walking"};
+    e[6]={6,"Jogging"};
+    for(i=0;i<=6;i++)
+        {
+        printf("%d%s",e[i].m,e[i].exercise);
+        }
     printf("How many of the following activities did you perform today?");
     scanf("%d",&n);
     printf("Exercise name\tDuration\tCalories burnt\n");
@@ -109,45 +69,45 @@ int main()
         printf("Enter exercise name & duration for which it is performed");
         scanf("%f%s",s[j].name,s[j].times);
 
-     for(j=k)
+     for(k=j;k<=j;k++)
         {
-            if(s[j].name=e[0].exercise)
+            if(s[j].name==e[0].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,((BMR*7)/(24*s[j].times)));
                 s[j].answer=((BMR*7)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[1].exercise)
+            else if(s[j].name==e[1].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[i].name,s[j].times,((BMR*5.5)/(24*s[j].times)));
                 s[j].answer=((BMR*5.5)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[2].exercise)
+            else if(s[j].name==e[2].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,(((BMR*3.3)/(24*s[j].times))));
                 s[j].answer=((BMR*3.3)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[3].exercise)
+            else if(s[j].name==e[3].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,((BMR*8.0)/(24*s[j].times)));
                 s[j].answer=((BMR*8.0)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[4].exercise)
+            else if(s[j].name==e[4].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,((BMR*3.0)/(24*s[j].times)));
                 s[j].answer=((BMR*3.0)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[5].exercise)
+            else if(s[j].name==e[5].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,((BMR*7)/(24*s[j].times)));
                 s[j].answer=((BMR*7)/(24*s[j].times));
             }
 
-            else if(s[j].name=e[6].exercise)
+            else if(s[j].name==e[6].exercise)
             {
                 printf("%s\t\t%f\t\t%f\n",s[j].name,s[j].times,((BMR*3.3)/(24*s[j].times)));
                 s[j].answer=((BMR*3.3)/(24*s[j].times));
